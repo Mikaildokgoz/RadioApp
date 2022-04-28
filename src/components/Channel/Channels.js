b48db7bb72fe5a0017fb6c8c274e691fe681b9e3
Mikaildokgoz <93521628+Mikaildokgoz@users.noreply.github.com>
2022-04-29 02:30:18 +0300

    Update Channels.js

--- src/components/Channel/Channels.js
+++ src/components/Channel/Channels.js
@@ -1,8 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./Channel.scss";
 
 const Channels = () => {
-  const [showImage, setShowImage] = useState(false);
 
   const withImage = () => {
     return (
